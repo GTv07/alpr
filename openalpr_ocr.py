7513f379fa5e836fbee7f97295e7a5361930a97b
Abdullah-Qutub <71206362+Abdullah-Qutub@users.noreply.github.com>
2022-05-25 16:27:58 +0300

    Update openalpr_ocr.py

--- openalpr_ocr.py
+++ openalpr_ocr.py
@@ -7,7 +7,7 @@ def ocr(IMAGE_PATH):
 	with open(IMAGE_PATH, 'rb') as image_file:
     		img_base64 = base64.b64encode(image_file.read())
 
-	url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=ksa&secret_key=%s' % (SECRET_KEY)  #Replace 'ind' with  your country code
+	url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=sudiarabia&secret_key=%s' % (SECRET_KEY)  #Replace 'ind' with  your country code
 	r = requests.post(url, data = img_base64)
 	try:
 		return(r.json()['results'][0]['plate'])
